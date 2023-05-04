1eafa58cb417acc5718d2508dde0fd11bee03c85
Salvador German <salvadorenriquegb@gmail.com>
2023-05-04 16:44:14 -0600

    Add request response body

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
+from pydantic import BaseModel
+
 from fastapi import FastAPI
 
 app = FastAPI()
 
+class Person(BaseModel):
+    name: str
+    last_name: str
+    age: int
+    hair_color: str | None = None
+    is_married: bool | None = None
+
 @app.get("/")
 def root():
     return {"message": "Hello world!"}
+
+# Request and response body
+@app.post("/people")
+def create_person(person: Person):
+    return person

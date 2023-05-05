d17b8f83b6a5e0673b9a75bcb15eda0258601116
Salvador German <salvadorenriquegb@gmail.com>
2023-05-05 13:18:27 -0600

    Add schema extra

--- main.py
+++ main.py
@@ -7,6 +7,24 @@ from fastapi import FastAPI, Query, Path, Body
 
 app = FastAPI()
 
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+    
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Foo",
+                "description": "A very nice Item",
+                "price": 35.4,
+                "tax": 3.3,
+            }
+        }
+
 class HairColor(Enum):
     white = "white"
     brown = "brown"
@@ -19,8 +37,18 @@ class Person(BaseModel):
     name: str = Field(default="Jhon", min_length=1, max_length=50)
     last_name: str | None = Field(default="Doe", min_length=1, max_length=50)
     age: int = Field(default=0, ge=0, le=200)
-    hair_color: HairColor
+    hair_color: HairColor = Field(default="black")
     is_married: bool = Field(default=False)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "name": "Jane",
+                "lastname": "Doe",
+                "age": 0,
+                "hair_color": "black"
+            }
+        }
 
 people_list = [Person(id=1, name="Salvador", last_name="German", age=23, hair_color="black", is_married=False),
                Person(id=2, name="Adrian", last_name="German", age=21, hair_color="black", is_married=False),
@@ -48,3 +76,12 @@ def show_person_detail(name: Annotated[str | None, Query(min_length=3, max_lengt
 @app.post("/people")
 def create_person(person: Annotated[Person, Body(...)]):
     return person
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    results = {"item_id": item_id, "item": item}
+    return results
+
+@app.post("/items")
+def create_item(item: Annotated[Item, Body(...)]):
+    return item

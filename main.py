08270edd252fd10327dc299cc53f61f5f9285f07
Salvador German <salvadorenriquegb@gmail.com>
2023-05-05 08:38:20 -0600

    Add validation for query parameters

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
+from typing import Annotated
 from pydantic import BaseModel
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 
 app = FastAPI()
 
 class Person(BaseModel):
-    name: str
-    last_name: str
-    age: int
+    id: int
+    name: str | None = None
+    last_name: str | None = None
+    age: int | None = None
     hair_color: str | None = None
     is_married: bool | None = None
+
+people_list = [Person(id=1, name="Salvador", last_name="German", age=23, hair_color="black", is_married=False),
+               Person(id=2, name="Adrian", last_name="German", age=21, hair_color="black", is_married=False),
+               Person(id=3, name="Gabriel", last_name="Hernandez", age=32, hair_color="blonde", is_married=False)]
 
 @app.get("/")
 def root():
     return {"message": "Hello world!"}
+
+@app.get("/people")
+def get_all_people():
+    return people_list
+
+@app.get("/people/{person_id}")
+def show_person():
+    return 
+
+@app.get("/people/")
+def show_person_detail(name: Annotated[str | None, Query(min_length=3, max_length=50)] = None):
+    return name
 
 # Request and response body
 @app.post("/people")
